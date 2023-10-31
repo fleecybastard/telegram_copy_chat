07b78c8d7c431175ecde283c492c364e036b8135
fleecybastard <illia09092003@gmail.com>
2023-11-01 00:42:26 +0200

    removed limit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from pyrogram import Client
 from pyrogram.enums import ChatType
 
@@ -31,21 +33,28 @@ async def pick_channel() -> int:
 
 
 async def resend_chat_history(chat_id: int, receiver_id: int) -> None:
-    async for message in app.get_chat_history(chat_id=chat_id, limit=10):
-        if message.video is not None:
-            await app.send_video(chat_id=receiver_id, video=message.video.file_id, caption=message.caption)
-        elif message.photo is not None:
-            await app.send_photo(chat_id=receiver_id, photo=message.photo.file_id, caption=message.caption)
-        elif message.voice is not None:
-            await app.send_voice(chat_id=receiver_id, voice=message.voice.file_id)
-        elif message.video_note is not None:
-            await app.send_video_note(chat_id=receiver_id, video_note=message.video_note.file_id)
-        elif message.document is not None:
-            await app.send_document(chat_id=receiver_id, document=message.document.file_id, caption=message.caption)
-        elif message.audio is not None:
-            await app.send_audio(chat_id=receiver_id, audio=message.audio.file_id, caption=message.caption)
-        elif message.text is not None:
-            await app.send_message(chat_id=receiver_id, text=message.text)
+    counter = 0
+    async for message in app.get_chat_history(chat_id=chat_id):
+        counter += 1
+        try:
+            if message.video is not None:
+                await app.send_video(chat_id=receiver_id, video=message.video.file_id, caption=message.caption)
+            elif message.photo is not None:
+                await app.send_photo(chat_id=receiver_id, photo=message.photo.file_id, caption=message.caption)
+            elif message.voice is not None:
+                await app.send_voice(chat_id=receiver_id, voice=message.voice.file_id)
+            elif message.video_note is not None:
+                await app.send_video_note(chat_id=receiver_id, video_note=message.video_note.file_id)
+            elif message.document is not None:
+                await app.send_document(chat_id=receiver_id, document=message.document.file_id, caption=message.caption)
+            elif message.audio is not None:
+                await app.send_audio(chat_id=receiver_id, audio=message.audio.file_id, caption=message.caption)
+            elif message.text is not None:
+                await app.send_message(chat_id=receiver_id, text=message.text)
+        except:
+            pass
+        if counter % 5 == 0:
+            await asyncio.sleep(5)
 
 
 async def main():
